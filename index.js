3ababfa99fea65a18344b14b7d43dd0520015dc4
VikashSingh-Dev <vikashsinghhvc@gmail.com>
2025-02-28 13:54:34 -0400

    obj lit
    Object literals

--- index.js
+++ index.js
@@ -1,35 +1,36 @@
-// callback-based array methods
-let arr = [12, 33, 4, 5, -4, 8, 19, 25];
-                
-//map() creates a new array from the elements of one without changing the old one
-function double(num){
-  return num * 2;
-}
-
-let doubledArr = arr.map(double); 
-console.log(doubledArr);
-
-function isOdd(element){
-  return element%2 !== 0; 
-}
-//Filter takes a test condition and returns only the element which 
-//make the condition true
-let odds = arr.filter(isOdd);
-console.log(odds);
-
-//Returns true or false if any of the elements of the array 
-//meets a specified condition
-function has5Factor(ele){
-  return ele % 5 === 0;
-}
-
-let hasFiveFactor = arr.some(has5Factor);
-console.log(hasFiveFactor);
-
-function intCompare(a, b){
- return a - b;
-}
-
-//sort function must return either 0, +ve, -ve
-let ascending = arr.sort(intCompare)
-console.log(ascending);
+// object literals !important
+let person = {
+    // key : value
+    "name": "John",
+    "age": 12
+  };
+  
+  //we can print the values of a key in two ways
+  console.log(person["age"]);//Using an index
+  console.log(person.name);//referencing the key as a property
+  
+  person["weight"] = 70;
+  
+  person.marks = [67, 34, 55, 89];
+  
+  let firstMark = person.marks[0];
+  
+  //mixing array and objects
+  let people = [
+    person,
+    {
+      name: "Jane",
+      age: 23,
+      marks: [51, 78, 99, 76]
+    }
+  ];
+  
+  let johnClone = {};//empty object
+  
+  //copying objects
+  Object.assign(johnClone, person);
+  
+  console.log(johnClone);
+  
+  
+  console.log(people[0].marks[0]);//what is printed?
