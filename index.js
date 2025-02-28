30cbd5ec5aaec2f8cfc67eb1314da63b918b06ec
VikashSingh-Dev <vikashsinghhvc@gmail.com>
2025-02-28 10:05:16 -0400

    string interpolation and task 3
    interpolation, output and deprecation

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
-let radius = 7;
-const pi = 3.14;
+let str1 ="Hello World";
+let str2 = 'Hello World';
+let str3 = `
+    hello 
+    world
+`;// template literal (backtick above tab key)
 
-let area = radius * radius * pi;
-console.log(area);  // this is how we output in js
-// use up arrow in terminal to get last entry
+console.log(str1, str2, str3);
+// template literals retain spaces in the tag and allow for multiline expressions
+
+// string interpolation
+let name = "bob";
+let age = 24;
+
+console.log(typeof(name))//string
+// name is deprecated as it is a special word in js and it needs to be deprecated to be able to be used freely
+console.log(typeof(age))//number
+
+console.log(`Hello my name is ${name}, I'm ${age} years old`);
+//Hello my name is bob, I'm 24 years old.
+
+console.log(`I was born in ${2020 - age}`)
+//I was born in 1966
