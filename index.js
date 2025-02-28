c5a0da18bf514e3ed7f2d95369db92f7021196d8
VikashSingh-Dev <vikashsinghhvc@gmail.com>
2025-02-28 10:57:09 -0400

    functions
    up to fallback values

--- index.js
+++ index.js
@@ -1,19 +1,23 @@
-// math
-let root = Math.sqrt(9);// 3
+// functions
+function hello() {
+    console.log("hello");
+  }
 
-let cubed  = Math.pow(2, 3);// 8
+function add(a, b){
+    return a + b;//undefined + undefined = NaN
+ }
+ 
+ let ans = add(5, 1);
+ console.log(ans); //6
+ 
+ let ans2 = add();
+ console.log(ans2); //NaN
 
-let roundDown = Math.floor(12.3453);// 12
-
-let roundUp = Math.ceil(12.3453);// 13
-
-let absolute = Math.abs(-34);// 34
-
-let randNum = Math.random();// random number between 0 and 1
-
-let pi = Math.pi;// pi in radians * 180 to convert to degrees
-
-//generate a random Integer between a supplied range
-function randInt(min, max){
-  return Math.floor(Math.random() * (max - min + 1) + min);
+ // default parameters
+ function add(a, b=10){
+    return a + b;
 }
+
+console.log(add(5,11)); // logs 16
+
+console.log(add(2));// logs 12
