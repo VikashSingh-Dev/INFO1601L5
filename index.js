d70d9dd155e584b6956e7f402a9f23e34796395b
VikashSingh-Dev <vikashsinghhvc@gmail.com>
2025-02-28 10:59:07 -0400

    first class funcs
    funcs

--- index.js
+++ index.js
@@ -1,23 +1,5 @@
-// functions
-function hello() {
-    console.log("hello");
-  }
+// first class functions
+let sum = 10
 
-function add(a, b){
-    return a + b;//undefined + undefined = NaN
- }
- 
- let ans = add(5, 1);
- console.log(ans); //6
- 
- let ans2 = add();
- console.log(ans2); //NaN
-
- // default parameters
- function add(a, b=10){
-    return a + b;
+function sum(){ //duplicate declaration error
 }
-
-console.log(add(5,11)); // logs 16
-
-console.log(add(2));// logs 12
