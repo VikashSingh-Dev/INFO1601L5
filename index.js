53066cb043f0ed26717817377fcca0f41e0bee8c
VikashSingh-Dev <vikashsinghhvc@gmail.com>
2025-02-28 10:20:30 -0400

    loops and task 4.3
    loops and task 4.3

--- index.js
+++ index.js
@@ -1,22 +1,24 @@
-console.log('1' == 1); //true
-console.log(1 == true); //true
-console.log("false" == false); // "false" is not false
-console.log("false" == true); // "false" is not true either
+// while and for
+let num =0;
 
-if("false")
-   console.log("Hello false!") //but "false" is true enough here
-  // for hard comparisons use === as == gives rise to undesirable events
+while(num < 10){
+  console.log(num);
+  num++;
+}
 
-//task 4.2
-console.log('1' === 1); //false
-console.log(1 === true); //false
-console.log("true" === true); //false
+for(let i=0; i <10; i ++){
+  console.log(i);
+}
 
-//logical operators
-let grade = 67;
-
-if( grade < 50){
-   console.log('Failed');
-}else{
-   console.log('Passed');
+// task 4.3
+for(let i=0; i< 50; i++){
+  if(i %3 === 0 && i%5 === 0){ // is divisible by 3 and 5?
+    console.log('fizzbuzz');
+  }else if(i%3 === 0){ // ok it's not divisible by both how about just 3?
+    console.log('fizz');
+  }else if(i%5 === 0){ //no? what about just 5 then?
+    console.log('buzz');
+  }else{              //so its not divisible by 3 or 5 then
+    console.log(i);
+  }
 }
