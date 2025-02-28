19e1d467eae76c100654c8d2c561bc996757d3a3
VikashSingh-Dev <vikashsinghhvc@gmail.com>
2025-02-28 11:04:10 -0400

    task 5
    callback functions

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
-// first class functions
-let sum = 10
-
-function sum(){ //duplicate declaration error
-}
+// callback functions
+function happyPrint(string){
+    console.log("😀: "+string);
+   }
+   
+   function sadPrint(string){
+    console.log("😢: "+string);
+   }
+   
+   //This high-order function adds 2 parameters and passes the answer to the callback
+   function add(a, b, callback){
+      let ans = a + b;
+      callback(ans);// call the callback and pass the answer to it
+   }
+   
+   //call add passing the callbacks to it
+   
+   add(5, 10, happyPrint);
+   add(11, 12, sadPrint);
