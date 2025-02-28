413e62ccbf1a2182431ff687aa9cc05fdcce3d37
VikashSingh-Dev <vikashsinghhvc@gmail.com>
2025-02-28 10:49:14 -0400

    math
    math functions

--- index.js
+++ index.js
@@ -1,33 +1,19 @@
-// date and math
+// math
+let root = Math.sqrt(9);// 3
 
-class CustomDate {
-    constructor() {
-        this.date = new Date();
-    }
+let cubed  = Math.pow(2, 3);// 8
 
-    getDate() {
-        return this.date;
-    }
+let roundDown = Math.floor(12.3453);// 12
+
+let roundUp = Math.ceil(12.3453);// 13
+
+let absolute = Math.abs(-34);// 34
+
+let randNum = Math.random();// random number between 0 and 1
+
+let pi = Math.pi;// pi in radians * 180 to convert to degrees
+
+//generate a random Integer between a supplied range
+function randInt(min, max){
+  return Math.floor(Math.random() * (max - min + 1) + min);
 }
-let customdate = new CustomDate();
-
-let nowTimestamp = Date.now();//store the current Date
-
-console.log(nowTimestamp);//unix timestamp in milliseconds 1581291269848
-
-let now = new Date(nowTimestamp);//today's Date object 
-
-//months go from 0 - 11
-let date = new Date(2019, 11, 17, 3, 24, 0);//December 17, 2019 03:24:00
-//printing the date
-
-console.log(date.toLocaleDateString("en-US"));//12/17/2019
-//1 is added to month because January is 0
-console.log(` ${now.getDate()} - ${now.getMonth() + 1} - ${now.getFullYear()} `); // 9 - 2 - 2020 
-
-//Calculate difference between two dates in milliseconds
-let difference = now - date;
-//convert milliseconds to days
-
-let daysBetween = difference * 1.15741e-8;
-console.log(daysBetween);//54.6740790437368
